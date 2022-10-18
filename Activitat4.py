74079f7aba1c2c6b2fb5f43dbc5837025aaddeb2
Eduard <edpufa@inspalamos.cat>
2022-10-18 18:52:42 +0200

    Update Activitat4.py

--- Activitat4.py
+++ Activitat4.py
@@ -1,5 +1,5 @@
-import csv
-with open('cars_data.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+import csv 
+with open("cars_data.csv",newline='') as csvFile:
+    render = csv.reader(csvFile)
+    for row in render:
+        print(row)
